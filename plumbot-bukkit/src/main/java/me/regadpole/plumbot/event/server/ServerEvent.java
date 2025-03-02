f1c9f8737c78640ded57198d972d4a31d0d0e7be
nachen520 <2720673822@qq.com>
2025-03-02 12:17:30 +0800

    Update ServerEvent.java

--- plumbot-bukkit/src/main/java/me/regadpole/plumbot/event/server/ServerEvent.java
+++ plumbot-bukkit/src/main/java/me/regadpole/plumbot/event/server/ServerEvent.java
@@ -136,7 +136,7 @@ public class ServerEvent implements Listener{
                 }
                 List<Long> groups = Config.getGroupQQs();
                 for (long groupID : groups){
-                    PlumBot.getBot().sendMsg(true, "玩家"+name+"加入游戏",groupID);
+                    PlumBot.getBot().sendMsg(true, "玩家"+name+"回到主城",groupID);
                 }
             });
         } else {
@@ -145,7 +145,7 @@ public class ServerEvent implements Listener{
             }
             List<Long> groups = Config.getGroupQQs();
             for (long groupID : groups){
-                PlumBot.getBot().sendMsg(true, "玩家"+name+"加入游戏",groupID);
+                PlumBot.getBot().sendMsg(true, "玩家"+name+"回到主城",groupID);
             }
         }
     }
@@ -160,7 +160,7 @@ public class ServerEvent implements Listener{
         }
         List<Long> groups = Config.getGroupQQs();
         for (long groupID : groups){
-            PlumBot.getBot().sendMsg(true, "玩家"+name+"退出游戏",groupID);
+            PlumBot.getBot().sendMsg(true, "玩家"+name+"离开主城",groupID);
         }
     }
 
